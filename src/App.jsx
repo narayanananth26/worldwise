3f93ef5214a633d1ffd23215a2c4782e8dd6cf8a
Ananth Narayan <narayanananth26@gmail.com>
2023-07-04 09:51:32 +0530

    implement programmatic navigation

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
+import Form from "./components/Form";
 
 const BASE_URL = "http://localhost:8000";
 function App() {
@@ -62,7 +63,7 @@ function App() {
 							/>
 						}
 					/>
-					<Route path="form" element={<p>Form</p>} />
+					<Route path="form" element={<Form />} />
 				</Route>
 				<Route path="*" element={<PageNotFound />} />
 			</Routes>
