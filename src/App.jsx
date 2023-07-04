5532c8e88d5d627673f1cc47ed7494cb459752aa
Ananth Narayan <narayanananth26@gmail.com>
2023-07-04 09:27:51 +0530

    implement dynamic routes w/ url params

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
 
 const BASE_URL = "http://localhost:8000";
 function App() {
@@ -51,6 +52,7 @@ function App() {
 							<CityList cities={cities} isLoading={isLoading} />
 						}
 					/>
+					<Route path="cities/:id" element={<City />} />
 					<Route
 						path="countries"
 						element={
