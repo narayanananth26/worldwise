b796113557060557300ea97560ae8058b8d62ebf
Ananth Narayan <narayanananth26@gmail.com>
2023-07-05 09:05:23 +0530

    finish city view

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -7,6 +7,7 @@ const CitiesContext = createContext();
 function CitiesProvider({ children }) {
 	const [cities, setCities] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
+	const [currentCity, setCurrentCity] = useState(false);
 
 	useEffect(function () {
 		async function fetchCities() {
@@ -25,11 +26,27 @@ function CitiesProvider({ children }) {
 		fetchCities();
 	}, []);
 
+	async function getCity(id) {
+		try {
+			setIsLoading(true);
+			const res = await fetch(`${BASE_URL}/cities/${id}`);
+			const data = await res.json();
+			setCurrentCity(data);
+		} catch (err) {
+			console.error(err);
+			alert("There was an error loading data");
+		} finally {
+			setIsLoading(false);
+		}
+	}
+
 	return (
 		<CitiesContext.Provider
 			value={{
 				cities,
 				isLoading,
+				currentCity,
+				getCity,
 			}}
 		>
 			{children}
