dee537070b7bb8f50875f7425503289d7b53ae43
Ananth Narayan <narayanananth26@gmail.com>
2023-07-07 10:29:30 +0530

    add context for fake auth

---
+++ src/contexts/FakeAuthContext.jsx
@@ -0,0 +1,60 @@
+import { createContext, useContext, useReducer } from "react";
+
+const AuthContext = createContext();
+
+const initialState = {
+	user: null,
+	isAuthenticated: false,
+};
+
+function reducer(state, action) {
+	switch (action.type) {
+		case "login":
+			return { ...state, user: action.payload, isAuthenticated: true };
+
+		case "logout":
+			return {
+				...state,
+				user: null,
+				isAuthenticated: false,
+			};
+
+		default:
+			throw new Error("Unknown action type");
+	}
+}
+
+const FAKE_USER = {
+	name: "Ananth",
+	email: "narayanananth26@gmail.com",
+	password: "abcd1234",
+	avatar: "https://avatars.githubusercontent.com/u/92174684?v=4",
+};
+
+function AuthProvider({ children }) {
+	const [{ user, isAuthenticated }, dispatch] = useReducer(
+		reducer,
+		initialState
+	);
+	function login(email, password) {
+		if (email === FAKE_USER.email && password === FAKE_USER.password)
+			dispatch({ type: "login", payload: FAKE_USER });
+	}
+
+	function logout() {
+		dispatch({ type: "logout" });
+	}
+	return (
+		<AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+			{children}
+		</AuthContext.Provider>
+	);
+}
+
+function useAuth() {
+	const context = useContext(AuthContext);
+	if (context === undefined)
+		throw new Error("AuthContext was used outside of AuthProvider");
+}
+
+export { AuthProvider, useAuth };
